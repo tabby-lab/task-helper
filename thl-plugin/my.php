5e4d8b1ea9f3ac8574549fde295249ca0a0b47a8
tabby-lab <tabby.garcia1@gmail.com>
2022-12-08 13:04:57 -0600

    the hotline wp plugin first push with most of the code

---
+++ thl-plugin/my.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>Local Host</title>
+</head>
+
+    </head>
+
+   <body>
+        
+        <?php
+        echo '<p>hi</p>';
+        ?>
+
+</body>
+</html>
